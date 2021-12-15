fbf45fd748471011d315851d31ec743b4dffec61
Greg Fenton <greg.fenton@gmail.com>
2021-12-15 13:53:21 -0500

    added (proper async!!) counter to screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hey C7 ain't this fun? <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <header className='App-header'>
+        {count}
+        <button
+          onClick={() => {
+            setTimeout(() => {
+              console.log(`setCount()!! ${Date()}`);
+              setCount((x) => x + 1);
+            }, 3000);
+            console.log(`CLICKED! ${count}`);
+          }}
         >
-          Learn React
-        </a>
+          click me
+        </button>
       </header>
     </div>
   );
